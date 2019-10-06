99f87dca1193675733af4647cd6bb6a9f3e4db19
pyc6eh4uk <ruslan.klimov1@gmail.com>
2019-10-07 00:04:12 +0300

    Distribution and matplotlib

--- task1.py
+++ task1.py
@@ -1,4 +1,5 @@
 from random import randint
+import matplotlib.pyplot as mpl
 import networkx as nx
 
 
@@ -13,6 +14,7 @@ class ErdesRenyiGraph:
         return nx.info(self.graph)
 
     def vertex_degree(self):
+        print(type(self.graph.degree(self.first_random_vertex)))
         return self.graph.degree(self.first_random_vertex)
 
     def neighbors(self):
@@ -22,23 +24,60 @@ class ErdesRenyiGraph:
         return nx.triangles(self.graph, self.first_random_vertex)
     
     def shortest_path(self):
-        return nx.shortest_path(self.graph, 
-                                source=self.first_random_vertex, 
-                                target=self.second_random_vertex)
-    
+        try:
+            return nx.shortest_path(self.graph, 
+                                    source=self.first_random_vertex, 
+                                    target=self.second_random_vertex)
+        except nx.NetworkXNoPath:
+            return f'Кратчайший путь между вершинами {self.first_random_vertex} '\
+                   f'и {self.second_random_vertex} не существует!'
+
+    def shortest_path_length(self):
+        try:
+            return nx.shortest_path_length(self.graph, 
+                                           source=self.first_random_vertex,
+                                           target=self.second_random_vertex)
+        except nx.NetworkXNoPath:
+            return f'Кратчайшее расстояние между вершинами {self.first_random_vertex} '\
+                   f'и {self.second_random_vertex} не существует!'
+
+    def distribution(self):
+        return [vertex[1] for vertex in nx.degree(self.graph)]
+
     def graph_stats(self):
         print(nx.info(self.graph))
         print(f'First random vertex: {self.first_random_vertex}')
         print(f'Second random vertex: {self.second_random_vertex}')
-        print(self.graph.degree(self.first_random_vertex))
-        print([vertex for vertex in self.graph.neighbors(self.first_random_vertex)])
-        print(nx.triangles(self.graph, self.first_random_vertex))
-        print(nx.shortest_path(self.graph, source=self.first_random_vertex, target=self.second_random_vertex))
-
+        print(f'Graph degree: {self.vertex_degree()}')
+        print(f'Neighbours: {[vertex for vertex in self.graph.neighbors(self.first_random_vertex)]}')
+        print(f'Triangles: {self.triangles()}')
+        print(f'Shortest path length: {self.shortest_path_length()}')
+        print(f'Shortest path: {self.shortest_path()}')
+        print(f'Graph distribution: {self.distribution()}')
 
 first_graph = ErdesRenyiGraph()
-first_graph.graph_stats()
-# print(nx.diameter(first_graph.graph))
+# first_graph.graph_stats()
+
+def bfs(g, v):
+    pass
+
+def diameterTree(g):
+    v, u, w = 0
+    d = bfs(g, v)
+    n = g.graph_length
+    for i in range(n):
+        if d[i] > d[u]:
+            u = i
+    bfs(g, u)
+    for i in range(n):
+        if d[i] > d[w]:
+            w = i
+    return d[w]
+
+mpl.hist(first_graph.distribution())
+mpl.show()
 
 second_graph = ErdesRenyiGraph(1000, 0.001)
-second_graph.graph_stats()
+# second_graph.graph_stats()
+mpl.hist(second_graph.distribution())
+mpl.show()
